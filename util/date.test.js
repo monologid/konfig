1e4713e136354b025f79fd891b00917c83294fe2
Ais <faris@monolog.id>
2020-02-21 09:36:37 +0000

    Add test for date util

--- util/date.test.js
+++ util/date.test.js
@@ -2,7 +2,8 @@ import DateUtil from './date';
 import moment from 'moment';
 
 test('toISOFormatString should return date in ISO format', () => {
-  let d = new DateUtil();
+  let currentDate = moment().format('YYYY-MM-DD');
+  let d = new DateUtil(currentDate);
   let date = d.toISOFormatString();
-  expect(date).not.toBe(moment().format('YYYY-MM-DD'));
+  expect(date).not.toBe(currentDate);
 })
