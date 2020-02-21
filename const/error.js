918b0e6692490bf998dd10f59b524fc4580c87c3
Ais <faris@monolog.id>
2020-02-21 09:34:56 +0000

    Add new error constants

--- const/error.js
+++ const/error.js
@@ -6,6 +6,7 @@ const Error = {
 
   // All errors related with Application
   AppNameMinLength: 'Application name should be more than 3 characters',
+  AppNameIsExist: 'Application name is exist',
 
   // All errors related with Environment
   EnvNameMinLength: 'Environment name should be at least 1 character',
