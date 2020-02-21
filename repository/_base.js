a0fbd38c7456296417a77b51d1cb314fe90d513f
Ais <faris@monolog.id>
2020-02-21 09:37:50 +0000

    Add mock repository class in base repository, and add query with deletedAt is null

--- repository/_base.js
+++ repository/_base.js
@@ -1,23 +1,27 @@
 import Database from '../core/database';
+import DateUtil from '../util/date';
 import uudv1 from 'uuid/v1';
 
 export default class BaseRepository {
   constructor(collection) {
     this.db = (new Database()).get();
     this.collection = this.db.collection(collection);
+    this.deletedAt = { $exists: false };
   }
 
   async findById(id) {
     return await this.collection
-      .findOne({ _id: id });
+      .findOne({ _id: id, deletedAt: this.deletedAt });
   }
 
-  async find({ query }) {
+  async find({ query, sort = {} }) {
+    query.deletedAt = this.deletedAt;
     return await this.collection
       .findOne(query);
   }
 
   async findAll({ query, sort = {}, skip = 0, limit = 10 }) {
+    query.deletedAt = this.deletedAt;
     return await this.collection
       .find(query)
       .sort(sort)
@@ -26,15 +30,43 @@ export default class BaseRepository {
       .toArray()
   }
 
-  async insert({ params }) {
+  async insert(params) {
+    const datetime = new DateUtil();
     params._id = uuidv1();
+    params.createdAt = datetime;
+    params.updatedAt = datetime;
     return await this.collection.insertOne(params);
   }
 
   async update({ query, params, upsert = false, isMulti = false }) {
+    query.deletedAt = this.deletedAt;
     if (isMulti) 
       return await this.collection.updateMany(query, { $set: params }, { upsert: isUpsert });
     
     return await this.collection.updateOne(query, { $set: params }, { upsert: isUpsert });
+  }
+}
+
+export class MockRepository {
+  constructor() { }
+
+  async findById(id) {
+    return {}
+  }
+
+  async find({ query, sort = {} }) {
+    return {}
+  }
+
+  async findAll({ query, sort = {}, skip = 0, limit = 10 }) {
+    return []
+  }
+
+  async insert(params) {
+    return {}
+  }
+
+  async update({ query, params, upsert = false, isMulti = false }) {
+    return {}
   }
 }
--- repository/application.js
+++ repository/application.js
@@ -6,4 +6,9 @@ export default class ApplicationRepository extends BaseRepository {
   constructor() { 
     super(collection);
   }
+
+  async findBySlug(slug) {
+    return await this.collection
+      .findOne({ slug: slug, deletedAt: this.deletedAt });
+  }
 }
